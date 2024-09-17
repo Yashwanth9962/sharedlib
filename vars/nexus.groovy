ac210f2c1f467bc5b803441255d2a8f9a6c3d002
Yashwanth9962 <korupoluvenkatasivasai98@gmail.com>
2024-09-17 16:10:32 +0530

    Create nexus.groovy

---
+++ vars/nexus.groovy
@@ -0,0 +1,1 @@
+nexusArtifactUploader artifacts: [[artifactId: 'my-web-app', classifier: '', file: 'target/my-web-app.war', type: 'war']], credentialsId: 'Nexus-credentials', groupId: 'Ashokit.in', nexusUrl: '13.201.186.54:8081/', nexusVersion: 'nexus3', protocol: 'http', repository: 'Artifact_Snapshot', version: '1.1-SNAPSHOT'
